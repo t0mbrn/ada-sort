f08a90997560f577e7b0e6abb4ec8674b992b093
Tom <mcelbschlumpf@gmail.com>
2022-04-28 16:53:31 +0200

    doc test with delare block

--- src/quick_sort.ads
+++ src/quick_sort.ads
@@ -8,10 +8,35 @@ package Quick_Sort is
   overriding
   function Name (This : in Object) return String is ("Quick Sort");
 
-  overriding
+--- ```
+--- declare
+---     Name : constant String := "Quick Sort";
+--- begin
+---     AUnit.Assertions.Assert
+---      (Actual   => Algorithm.Name,
+---       Expected => Name,
+---       Message  => "Name did not match expected. Expected: " & Name);
+--- end;
+--- ```
+
+    overriding function Sort
   function Sort
     (This       : in Object;
      Sort_Array : in Random_Array.Object)
      return Random_Array.Object;
+
+--- ```
+--- declare
+---     Input  : Random_Array.Object := Random_Array.Functions.Generate;
+---     Output : Random_Array.Object (Input'Range);
+--- begin
+---
+---     Output := Algorithm.Sort (Input);
+---
+---     AUnit.Assertions.Assert
+---      (Condition => Random_Array.Functions.Is_Sorted (Output),
+---       Message   => "The array was not sorted correctly.");
+--- end;
+--- ```
 
 end Quick_Sort;
